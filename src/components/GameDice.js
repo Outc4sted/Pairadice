03348aa7c2ec7a66f1acbdc672fb5887afe71e31
Justen <outc4sted@gmail.com>
2016-04-23 21:43:49 -0400

    first pass on gamedice and some tweaks to gameinfobar

--- src/components/GameDice.js
+++ src/components/GameDice.js
@@ -1,24 +1,56 @@
 import React, { PropTypes } from 'react';
+import { Row, Col } from 'react-flexbox-grid/lib';
+import RaisedButton from 'material-ui/RaisedButton';
 
+
+const styles = {
+  gameDiceRow: {
+    marginTop: '10px',
+    height: '140px'
+  },
+  gameDice: {
+    width: '100px',
+    height: '100px',
+    border: '4px solid #000',
+    WebkitBorderRadius: '10px',
+    MozBorderRadius: '10px',
+    borderRadius: '10px',
+    padding: '1px'
+  },
+  group1: {
+    border: '8px solid #F00',
+    WebkitBorderRadius: '20px',
+    MozBorderRadius: '20px',
+    borderRadius: '20px',
+    padding: '2 0'
+  },
+  group2: {
+    border: '8px solid #0F0',
+    WebkitBorderRadius: '20px',
+    MozBorderRadius: '20px',
+    borderRadius: '20px',
+    padding: '2 0'
+  }
+};
 
 const GameDice = ({ gameDice, toggleDice, gameStarted }) => (
-  <div>
-    <h2>Game Dice</h2>
-
+  <Row style={styles.gameDiceRow} around="xs" middle="xs">
     {gameDice.map((dice, index) => {
       return (
-        <div key={index} onClick={() => gameStarted && toggleDice(index)}>
-          <span>[Dice {index}]</span>
-          <div>
-            <span>val: {dice.value}</span>
-            &nbsp;
-            <span>group: {dice.group}</span>
+        <Col xs={2} key={index}>
+          <div style={dice.group ? (dice.group === 1 ? styles.group1 : styles.group2) : null}>
+            <RaisedButton style={styles.gameDice} onClick={() => gameStarted && toggleDice(index)}>
+              <Row>
+                <Col xs>
+                  <span>{dice.value}</span>
+                </Col>
+              </Row>
+            </RaisedButton>
           </div>
-          <br />
-        </div>
+        </Col>
       );
     })}
-  </div>
+  </Row>
 );
 
 GameDice.propTypes = {
--- src/components/GameInfoBar.js
+++ src/components/GameInfoBar.js
@@ -1,20 +1,37 @@
 import React, { PropTypes } from 'react';
 import { Row, Col } from 'react-flexbox-grid/lib';
 
+const styles = {
+  row: {
+    height: '50px'
+  },
+  left: {
+    textAlign: 'left'
+  },
+  right: {
+    textAlign: 'right'
+  }
+};
 
 const GameInfoBar = ({gameInfoBar: { round, totalPoints, groupTotals }}) => (
-  <Row>
-    <Col xs>
-      <span>Round: {round}</span>
+  <Row style={styles.row} middle="xs">
+    <Col xs={4}>
+      <h3 style={styles.left}>Round: {round}</h3>
     </Col>
-    <Col xs>
-      <span>Group 1 Total: {groupTotals[0]}</span>
+    <Col xs={3}>
+      <h2 style={styles.right}>{totalPoints} points</h2>
     </Col>
-    <Col xs>
-      <span>Group 2 Total: {groupTotals[1]}</span>
-    </Col>
-    <Col xs>
-      <span>Total Points: {totalPoints}</span>
+    <Col xs={5}>
+      <Row>
+        <Col xs>
+          <div style={styles.right}>Group 1: {groupTotals[0]}</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs>
+          <div style={styles.right}>Group 2: {groupTotals[1]}</div>
+        </Col>
+      </Row>
     </Col>
   </Row>
 );
